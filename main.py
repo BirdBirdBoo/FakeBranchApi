2b8629d9a23c8d6a776c7f23e08cb3fad87ade43
Maksym-Yurii.Rudko <maksym-iurii.rudko.pz.2020@lpnu.ua>
2023-05-14 17:45:17 +0300

    Added more branches

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ app = Flask(__name__)
 # Sample data
 offices = [
     {"id": 1, "city": "Kyiv", "rent": 15000},
-    {"id": 2, "city": "Lviv", "rent": 12000},
-    {"id": 3, "city": "Kharkiv", "rent": 20000},
-    {"id": 4, "city": "Odessa", "rent": 18000},
-    {"id": 5, "city": "Dnipro", "rent": 16000},
-    {"id": 6, "city": "Zaporizhia", "rent": 14000},
-    {"id": 7, "city": "Vinnytsia", "rent": 10000},
-    {"id": 8, "city": "Poltava", "rent": 11000},
-    {"id": 9, "city": "Chernivtsi", "rent": 13000},
-    {"id": 10, "city": "Lutsk", "rent": 9000},
+    {"id": 2, "city": "Lviv", "rent": 8000},
+    {"id": 3, "city": "Odesa", "rent": 6000},
+    {"id": 4, "city": "Kyiv", "rent": 20000},
+    {"id": 5, "city": "Kharkiv", "rent": 9000},
+    {"id": 6, "city": "Dnipro", "rent": 12000},
+    {"id": 7, "city": "Kyiv", "rent": 25000},
+    {"id": 8, "city": "Lviv", "rent": 10000},
+    {"id": 9, "city": "Kyiv", "rent": 18000},
+    {"id": 10, "city": "Odesa", "rent": 7000},
+    {"id": 11, "city": "Kharkiv", "rent": 8000},
+    {"id": 12, "city": "Dnipro", "rent": 15000},
+    {"id": 13, "city": "Kyiv", "rent": 22000},
+    {"id": 14, "city": "Lviv", "rent": 12000},
+    {"id": 15, "city": "Kyiv", "rent": 28000}
 ]
 
 
